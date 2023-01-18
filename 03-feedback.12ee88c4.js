86fe46c5d2a70601da99439bf02883043cb628a6
IhorLavrenchyk <110937655+IhorLavrenchyk@users.noreply.github.com>
2023-01-18 18:32:43 +0000

    Deploying to gh-pages from @ IhorLavrenchyk/goit-js-hw-08@cbc50d2f1e4c044312810b17dcbba3eb9ee10440 🚀

---
+++ 03-feedback.12ee88c4.js
@@ -0,0 +1,2 @@
+!function(){var e={form:document.querySelector("feedback-form"),textarea:document.querySelector("message")};e.form.addEventListener("submit",(function(e){e.preventDefault(),console.log("send"),e.currentTarget.reset()})),e.textarea.addEventListener("input",onTextareaInput)}();
+//# sourceMappingURL=03-feedback.12ee88c4.js.map
---
+++ 03-feedback.5d6ec973.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector("feedback-form"),textarea:document.querySelector("message")};e.form.addEventListener("submit",(function(e){e.preventDefault(),console.log("send"),e.currentTarget.reset()})),e.textarea.addEventListener("input",onTextareaInput);
+//# sourceMappingURL=03-feedback.5d6ec973.js.map
