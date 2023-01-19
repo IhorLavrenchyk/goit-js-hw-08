493a2848205f110494dc36cbf7d84a37e1c2f02a
IhorLavrenchyk <110937655+IhorLavrenchyk@users.noreply.github.com>
2023-01-19 09:40:17 +0200

    45

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,5 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-import lodash from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 
@@ -20,23 +19,17 @@ const onPlay = function (data) {
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-// ('videoplayer-current-time');
-
 const currentTime = localStorage.getItem('videoplayer_current_time');
 
 player
   .setCurrentTime(currentTime)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
+  .then(function (seconds) {})
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
         break;
 
       default:
-        // some other error occurred
         break;
     }
   });
